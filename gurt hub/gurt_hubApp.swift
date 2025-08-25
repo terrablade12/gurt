13947256bf12563d8e260ac0692569ff87645f2d
terrablade12 <terrablade1212@gmail.com>
2025-08-25 16:20:57 +0800

    Initial Commit

---
+++ gurt hub/gurt_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  gurt_hubApp.swift
+//  gurt hub
+//
+//  Created by Iain Tan Qi yuan on 25/8/25.
+//
+
+import SwiftUI
+
+@main
+struct gurt_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
